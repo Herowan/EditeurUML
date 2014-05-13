8743dc3e3ce15109c9244a8b932d3cda80f214c5
Dashell <galasxalain@gmail.com>
2014-05-13 16:45:35 +0200

    Ajout de l'attribut position , contenant les coordonnées de l'objet. Ajout de getteur renvoyant le x et le y des coordonnées Ajout des méthodes comparant la position sur chacun des axes de deux objet UML

--- EditeurUML/src/modele/ObjetUML.java
+++ EditeurUML/src/modele/ObjetUML.java
@@ -7,12 +7,14 @@ public class ObjetUML {
 	private ArrayList<Methode> listeMethode;
 	private ArrayList<Attribut> listeAttribut;
 
-	
+	private Coordonnees position;
 	
 	
 	public ObjetUML(){
 		listeMethode= new ArrayList<Methode>();
 		listeAttribut=new ArrayList<Attribut>();
+		//a changer , les coordonnées seront pas 0,0 par default
+		position=new Coordonnees(0,0);
 	}
 	
 	public void setNom(String nom){
@@ -40,6 +42,30 @@ public class ObjetUML {
 	int tailleListeAttribut(){
 		return listeAttribut.size();
 	}
+	//Gestion des coordonnées
+	public int getX(){
+		return position.getX();
+	}
+	public int getY(){
+		return position.getY();
+	}
 	
+	//Gestion de la position d'un objet
+	/**
+	 * 
+	 * @param obj UML Object to Compare position on the vertical axe
+	 * @return a positive number if the main object is above , negative else
+	 */
+	public int isAbove(ObjetUML obj){
+		return obj.getY()-this.getY();
+	}
+	/**
+	 * 
+	 * @param obj UML Object to Compare position on the horizontal axe
+	 * @return a positive number if the main object is on the right, negative else
+	 */
+	public int isOnTheRigth(ObjetUML obj){
+		return this.getX()-obj.getX();
+	}
 	
 }
