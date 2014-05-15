0b73e007a6eb09bb327ebc5254d0d900eae5fae2
Dashell <galasxalain@gmail.com>
2014-05-15 22:01:58 +0200

    Ajout de la classe Association dans le modele

---
+++ EditeurUML/model/Association.java
@@ -0,0 +1,46 @@
+package model;
+
+public class Association {
+
+	ObjectUML firstObject;
+	ObjectUML lastObject;
+	int typeOfAssociation;
+	
+	
+	public Association(ObjectUML firstObject, ObjectUML lastObject,int typeOfAssociation){
+		this.firstObject=firstObject;
+		this.lastObject=lastObject;
+		this.typeOfAssociation=typeOfAssociation;
+	}
+
+
+	public ObjectUML getFirstObject() {
+		return firstObject;
+	}
+
+
+	public void setFirstObject(ObjectUML firstObject) {
+		this.firstObject = firstObject;
+	}
+
+
+	public ObjectUML getLastObject() {
+		return lastObject;
+	}
+
+
+	public void setLastObject(ObjectUML lastObject) {
+		this.lastObject = lastObject;
+	}
+
+
+	public int getTypeOfAssociation() {
+		return typeOfAssociation;
+	}
+
+
+	public void setTypeOfAssociation(int typeOfAssociation) {
+		this.typeOfAssociation = typeOfAssociation;
+	}
+	
+}
