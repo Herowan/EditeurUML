a96dac58c03c3988b2e38ed4cc2c11f4811d7380
emelieum <emelieum@10.19.3.242>
2014-05-28 13:06:57 +0200

    Java doc de Association terminer, je rajouterais des que du neuf seras fais

--- EditeurUML/model/Association.java
+++ EditeurUML/model/Association.java
@@ -4,47 +4,68 @@ import java.io.Serializable;
 
 public class Association implements Serializable{
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	ObjectUML firstObject;
 	ObjectUML lastObject;
 	int typeOfAssociation;
 	
-	
+	/**
+	 * Construct an Association between firstObject and lastObject with typeOfAssociation. 
+	 * @param firstObject : the first object attached to the Association
+	 * @param lastObject : the last object attached to the Association
+	 * @param typeOfAssociation : the type of the corresponding Association
+	 */
 	public Association(ObjectUML firstObject, ObjectUML lastObject,int typeOfAssociation){
 		this.firstObject=firstObject;
 		this.lastObject=lastObject;
 		this.typeOfAssociation=typeOfAssociation;
 	}
 
-
+	/**
+	 * return the firstObjetct of the Association
+	 * @return return the firstObject of the Association
+	 */
 	public ObjectUML getFirstObject() {
 		return firstObject;
 	}
 
-
+	/**
+	 * Defines the first objects of the Association 
+	 * @param firstObject : the first object attached to the Association
+	 */
 	public void setFirstObject(ObjectUML firstObject) {
 		this.firstObject = firstObject;
 	}
 
-
+	/**
+	 * return the lastObjetct of the Association
+	 * @return return the last object attached to the Association
+	 */
 	public ObjectUML getLastObject() {
 		return lastObject;
 	}
 
-
+	/**
+	 * Defines the last objects of the Association
+	 * @param lastObject : the last object attached to the Association
+	 */
 	public void setLastObject(ObjectUML lastObject) {
 		this.lastObject = lastObject;
 	}
 
-
+	/**
+	 * return the type of the corresponding Association
+	 * @return return the type of the corresponding Association 
+	 */
 	public int getTypeOfAssociation() {
 		return typeOfAssociation;
 	}
 
-
+	/**
+	 * Defines the type of the corresponding Association 
+	 * @param typeOfAssociation : the type of the corresponding Association
+	 */
 	public void setTypeOfAssociation(int typeOfAssociation) {
 		this.typeOfAssociation = typeOfAssociation;
 	}
